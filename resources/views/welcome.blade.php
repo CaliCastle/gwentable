7a3cfb995d936319ea724e845f3254d498703367
Cali Castle <calicastlemac@Calis-MacBook-Pro-15.local>
2017-03-11 15:40:05 -0800

    Alter links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Gwentable</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -83,8 +83,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Official Page</a>
-                    <a href="https://github.com/laravel/laravel">GwentDB</a>
+                    <a href="https://playgwent.com">Official Page</a>
+                    <a href="http://gwentdb.com">GwentDB</a>
                 </div>
             </div>
         </div>
